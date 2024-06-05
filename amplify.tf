dc2402ddb213f59ae83c7e07f1c4dbdc7748453d
Abdala <clahimoha1000@gmail.com>
2024-06-05 17:30:30 +0300

    modified the index.html

--- amplify.tf
+++ amplify.tf
@@ -2,6 +2,31 @@ resource "aws_amplify_app" "my-amplify-app" {
   name       = "wildrydes-app"
   repository = "https://github.com/clahi/serverless-web-app-terraform.git"
 
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - yarn install
+        build:
+          commands:
+            - yarn run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
   access_token = var.access-token
 }
 
