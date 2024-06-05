940eb33a70fbc5b370d4e30a2521f6671c1e7ca5
Abdala <clahimoha1000@gmail.com>
2024-06-05 16:02:55 +0300

    initial web files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_cmNu8SFAC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6qdujfj576i93gbkfjoacpgo9v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://576qvoqc44.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
