edbb5575909381105de129c93b9386f6215e1ecd
Abdala <clahimoha1000@gmail.com>
2024-06-05 19:41:10 +0300

    triggered amplify to run automatically

--- amplify.tf
+++ amplify.tf
@@ -1,36 +1,39 @@
 resource "aws_amplify_app" "my-amplify-app" {
-  name       = "wildrydes-app"
-  repository = "https://github.com/clahi/serverless-web-app-terraform.git"
+  name         = "wildrydes-app"
+  repository   = "https://github.com/clahi/serverless-web-app-terraform.git"
+  access_token = var.access-token
 
-  build_spec = <<-EOT
-    version: 0.1
-    frontend:
-      phases:
-        preBuild:
-          commands:
-            - yarn install
-        build:
-          commands:
-            - yarn run build
-      artifacts:
-        baseDirectory: build
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - node_modules/**/*
-  EOT
-
+  # The default rewrites and redirects added by the Amplify Console.
   custom_rule {
     source = "/<*>"
     status = "404"
     target = "/index.html"
   }
 
-  access_token = var.access-token
+  # Auto Branch Creattion
+  enable_auto_branch_creation = true
+
+  # The default patterns added by the Amplify Console.
+  auto_branch_creation_patterns = [
+    "*",
+    "*/**",
+  ]
+
+  auto_branch_creation_config {
+    # Enable auto build for the created branch.
+    enable_auto_build = true
+  }
 }
 
-resource "aws_amplify_branch" "amplify-branch" {
+resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.my-amplify-app.id
   branch_name = var.branch_name
+
+  stage = "PRODUCTION"
+}
+
+resource "aws_amplify_webhook" "main" {
+  app_id      = aws_amplify_app.my-amplify-app.id
+  branch_name = aws_amplify_branch.main.branch_name
+  description = "triggermaster"
 }
