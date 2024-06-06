8087703122c63a3cb58bffe351369d5e84188c86
Abdala <clahimoha1000@gmail.com>
2024-06-06 20:07:43 +0300

    created a rest api gatway which is integrated with lambda function

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: aws_cognito_user_pool.WildRydes.id, // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: aws_amplify_app.my-amplify-app.id, // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_z4GvK7hGk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4h35mqgdckil21j7fh0ljlgg7r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://97i5ptgjlj.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://95gdcmib3g.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
