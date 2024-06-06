c72da3e902bc1c07b2a2aaf064a914827577544b
Abdala <clahimoha1000@gmail.com>
2024-06-06 11:31:36 +0300

    created a cognito user pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_daS4Z3Iot', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '25g138s1s7ij99q2dguc13qdg2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
