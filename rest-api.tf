8b2162f780a156cb8e03aff62a6dc675ae05cb6b
Abdala <clahimoha1000@gmail.com>
2024-06-06 17:58:37 +0300

    created a rest api gatway which is integrated with lambda function

--- rest-api.tf
+++ rest-api.tf
@@ -78,8 +78,8 @@ resource "aws_api_gateway_method" "options" {
   rest_api_id   = aws_api_gateway_rest_api.WildRydes.id
   resource_id   = aws_api_gateway_resource.ride.id
   http_method   = "OPTIONS"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.WildRydes.id
+  authorization = "NONE"
+#   authorizer_id = aws_api_gateway_authorizer.WildRydes.id
 }
 
 resource "aws_api_gateway_integration" "options_integration" {
