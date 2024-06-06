b55b813cfb739349ece5c933a97f33a8b2ce47ac
Abdala <clahimoha1000@gmail.com>
2024-06-06 16:11:51 +0300

    created a rest api gatway which is integrated with lambda function

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_D7hMe5JW2', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'qtp4tfqaoe1k6r0cdntd2v518', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_BZbgQiAHQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5jk0f13tbh99lssf1hghcob624', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://fsq4cjw4gc.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://6p0ze7rpri.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
--- rest-api.tf
+++ rest-api.tf
@@ -74,9 +74,64 @@ resource "aws_api_gateway_integration_response" "proxy" {
   ]
 }
 
+resource "aws_api_gateway_method" "options" {
+  rest_api_id   = aws_api_gateway_rest_api.WildRydes.id
+  resource_id   = aws_api_gateway_resource.ride.id
+  http_method   = "OPTIONS"
+  authorization = "COGNITO_USER_POOLS"
+  authorizer_id = aws_api_gateway_authorizer.WildRydes.id
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.WildRydes.id
+  resource_id             = aws_api_gateway_resource.ride.id
+  http_method             = aws_api_gateway_method.options.http_method
+  integration_http_method = "OPTIONS"
+  type                    = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options_response" {
+  rest_api_id = aws_api_gateway_rest_api.WildRydes.id
+  resource_id = aws_api_gateway_resource.ride.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.WildRydes.id
+  resource_id = aws_api_gateway_resource.ride.id
+  http_method = aws_api_gateway_method.options.http_method
+  status_code = aws_api_gateway_method_response.options_response.status_code
+
+  # cors
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [
+    aws_api_gateway_method.options,
+    aws_api_gateway_integration.options_integration,
+  ]
+}
+
+
+
+
 resource "aws_api_gateway_deployment" "deploymet" {
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration.options_integration,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.WildRydes.id
