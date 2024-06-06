7933e5b95fb87258af1234f1338385b21dcf0380
Abdala <clahimoha1000@gmail.com>
2024-06-06 18:19:12 +0300

    created a rest api gatway which is integrated with lambda function

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://6p0ze7rpri.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://1eie2cgnwb.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
